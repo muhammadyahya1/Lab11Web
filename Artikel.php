4244161a05c3a28eaac68f7460e8391fb7996cc0
muhammadyahya1 <81598272+muhammadyahya1@users.noreply.github.com>
2021-06-28 00:08:26 +0700

    Add files via upload

---
+++ Artikel.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controllers;
+
+use App\Models\ArtikelModel;
+
+class Artikel extends BaseController
+{
+    public function index() 
+    {
+        $title = 'Daftar Artikel';
+        $model = new ArtikelModel();
+        $artikel = $model->findAll();
+        return view('artikel/index', compact('artikel', 'title'));
+    }
+}
